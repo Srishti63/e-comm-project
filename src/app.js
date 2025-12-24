02cf4c6790020af01adfe615eb4b9bacd13ac742
Srishti <srishtigupta28aug@gmail.com>
2025-12-24 14:00:59 +0530

    #

--- src/app.js
+++ src/app.js
@@ -1,5 +1,18 @@
 import express from "express";
+import cors from "cors";
+import cookieparser from "cookies-parser";
 
 const app = express();
+
+app.use(cors({
+    origin: process.env.COR_ORIGIN,
+    credentials : true
+}))
+app.use(express.json({limit : "16kb"}))
+app.use(express.urlencoded({extended: true, limit:"16kb"}))
+app.use(express.static("public"))
+app.use(cookieparser())
+
+
 
 export { app }
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,7 @@
+const asynHandler = (requestHandler)=>{
+    return (req,resp,next)=>{
+    Promise.resolve(requestHandler(req,resp,next)).catch((err)=>(next(err)))
+    }
+}
+
+export { asynHandler }
